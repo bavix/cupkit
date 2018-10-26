c5f1dd279a16de67ef9dd3ec5e31d85b0f4ab5e7
Babichev Maxim <babichev@mail.tvil.ru>
2018-10-26 15:09:16 +0300

    update client

--- src/Client.php
+++ src/Client.php
@@ -34,7 +34,65 @@ class Client
      */
     public function getBucket(string $name, array $include = []): array
     {
-        return $this->single(\sprintf('/api/bucket/%s', $name), \compact($include));
+        return $this->fetch(\sprintf('/api/bucket/%s', $name), \compact($include));
+    }
+
+    /**
+     * @param string $name
+     * @return array
+     */
+    public function createBucket(string $name): array
+    {
+        return $this->create('/api/bucket', \compact('name'));
+    }
+
+    /**
+     * @param string $name
+     * @return bool
+     */
+    public function dropBucket(string $name): bool
+    {
+        return $this->delete(\sprintf('/api/bucket/%s', $name));
+    }
+
+    /**
+     * @param string $bucket
+     * @return \Generator
+     */
+    public function getViews(string $bucket): \Generator
+    {
+        return $this->each(\sprintf('/api/bucket/%s/view', $bucket));
+    }
+
+    /**
+     * @param string $bucket
+     * @param string $name
+     * @param array $include
+     * @return array
+     */
+    public function getView(string $bucket, string $name, array $include = []): array
+    {
+        return $this->fetch(\sprintf('/api/bucket/%s/view/%s', $bucket, $name), \compact($include));
+    }
+
+    /**
+     * @param string $bucket
+     * @param array $data
+     * @return array
+     */
+    public function createView(string $bucket, array $data): array
+    {
+        return $this->create(\sprintf('/api/bucket/%s/view', $bucket), $data);
+    }
+
+    /**
+     * @param string $bucket
+     * @param string $name
+     * @return bool
+     */
+    public function dropView(string $bucket, string $name): bool
+    {
+        return $this->delete(\sprintf('/api/bucket/%s/view/%s', $bucket, $name));
     }
 
     /**
@@ -48,11 +106,13 @@ class Client
 
     /**
      * @param string $bucket
-     * @return \Generator
+     * @param string $uuid
+     * @param array $include
+     * @return array
      */
-    public function getViews(string $bucket): \Generator
+    public function getImage(string $bucket, string $uuid, array $include = []): array
     {
-        return $this->each(\sprintf('/api/bucket/%s/view', $bucket));
+        return $this->fetch(\sprintf('/api/bucket/%s/image/%s', $bucket, $uuid), $include);
     }
 
     /**
@@ -82,7 +142,7 @@ class Client
      * @param array $includes
      * @return array
      */
-    protected function single(string $path, array $includes): array
+    protected function fetch(string $path, array $includes): array
     {
         $include = \implode(',', $includes);
         $response = $this->identity->get($path, [
@@ -91,6 +151,32 @@ class Client
 
         $received = new Received($response);
         return $received->asArray();
+    }
+
+    /**
+     * @param string $path
+     * @param array $data
+     * @return array
+     */
+    protected function create(string $path, array $data): array
+    {
+        $response = $this->identity->post($path, $data);
+        $received = new Received($response);
+        return $received->asArray();
+    }
+
+    /**
+     * @param string $path
+     * @return bool
+     */
+    protected function delete(string $path): bool
+    {
+        try {
+            $this->identity->delete($path);
+            return true;
+        } catch (\Throwable $throwable) {
+            return false;
+        }
     }
 
 }
